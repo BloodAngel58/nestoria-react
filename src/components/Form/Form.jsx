8dce1eaa1face660634f3856c774efcc4dc04552
BloodAngel58 <bloodange2013@yandex.ru>
2019-07-01 11:38:05 +0300

    add react

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+class Form extends React.Component {
+
+    render() {
+        return (
+            <React.Fragment>
+                <h1>HELLO</h1>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Form;
